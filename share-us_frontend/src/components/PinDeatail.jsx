dd9c2d0340c0a1c2610c7433274f094b7bbc05c8
shafin_shant0 <shafinshanto9@gmail.com>
2022-01-20 23:57:10 +0600

    Navbar_complited

---
+++ share-us_frontend/src/components/PinDeatail.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PinDeatail() {
+  return(
+    <div>Details</div>
+  )
+}
+
+export default PinDeatail;
--- share-us_frontend/src/container/Pins.jsx
+++ share-us_frontend/src/container/Pins.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import CreatePin from '../components/CreatePin';
+import Feed from '../components/Feed';
+import NavBar from '../components/NavBar';
+import PinDeatail from '../components/PinDeatail';
+import Search from '../components/Search';
 
-const Pins = () => {
+
+const Pins = ({user}) => {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
-    <div>
-        pins
+    <div className='px-2 md:px-5'>
+        <div className='bg-gray-50'>
+          <NavBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} user={user} />
+        </div>
+        <div className='h-full'>
+          <Routes>
+            <Route path='/' element={<Feed/>} />
+            <Route path='/category/:categoryId' element={<Feed/>} />
+            <Route path='/pin-detail/:pinId' element={<PinDeatail user={user}/>} />
+            <Route path='/create-pin' element={<CreatePin user={user} />} />
+            <Route path='/search' element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />} />
+          </Routes>
+        </div>
+       
     </div>
 
   )
