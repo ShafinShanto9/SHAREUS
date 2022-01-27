8c7a2f0aa12cf371d15e4568ef220a18565e4b2f
shafin_shant0 <shafinshanto9@gmail.com>
2022-01-27 17:36:25 +0600

    fixed_search_issue

--- share-us_frontend/src/components/Search.jsx
+++ share-us_frontend/src/components/Search.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import MasonaryLayout from './MasonaryLayout';
+import Spinner from './Spinner';
 
-const Search = () => {
+
+const Search = ({ searchTerm }) => {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchTerm !== '') {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
   return (
-  <div>Search</div>
+    <div>
+
+      {loading && <Spinner message="Searching pins" />}
+      {pins?.length !== 0 && <MasonaryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== '' && !loading && (
+        <div className="mt-10 text-center text-xl ">No Pins Found!</div>
+      )}
+    </div>
   );
 };
 
