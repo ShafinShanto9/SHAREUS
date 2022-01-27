47e132940eaabb17b1ffc5d64ed03f0a62b7d1e9
shafin_shant0 <shafinshanto9@gmail.com>
2022-01-27 16:32:09 +0600

    user_profile_done

--- share-us_frontend/src/components/UserProfile.jsx
+++ share-us_frontend/src/components/UserProfile.jsx
@@ -9,6 +9,10 @@ import { client } from '../client';
 import Spinner from './Spinner';
 import MasonaryLayout from './MasonaryLayout';
 
+
+const activeBtnStyles = 'bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none';
+const notActiveBtnStyles = 'bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none';
+
 const UserProfile = () => {
   const [user, setUser] = useState();
   const [pins, setPins] = useState();
@@ -23,6 +27,28 @@ const UserProfile = () => {
       setUser(data[0]);
     });
   }, [userId]);
+
+  useEffect(() => {
+    if (text === 'Created') {
+      const createdPinsQuery = userCreatedPinsQuery(userId);
+
+      client.fetch(createdPinsQuery).then((data) => {
+        setPins(data);
+      });
+    } else {
+      const savedPinsQuery = userSavedPinsQuery(userId);
+
+      client.fetch(savedPinsQuery).then((data) => {
+        setPins(data);
+      });
+    }
+  }, [text, userId]);
+
+  const logout = () => {
+    localStorage.clear();
+
+    navigate('/login');
+  };
   
   if (!user) return <Spinner message="Loading profile" />;
   return(
@@ -41,7 +67,62 @@ const UserProfile = () => {
               alt="user-pic"
             />
           </div>
+          <h1 className="font-bold text-3xl text-center mt-3">
+            {user.userName}
+          </h1>
+
+          <div className="absolute top-0 z-1 right-0 p-2">
+            {userId === user._id && (
+              <GoogleLogout
+                clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+                render={(renderProps) => (
+                  <button
+                    type="button"
+                    className=" bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                  >
+                    <AiOutlineLogout color="red" fontSize={21} />
+                  </button>
+                )}
+                onLogoutSuccess={logout}
+                cookiePolicy="single_host_origin"
+              />
+            )}
           </div>
+          </div>
+          <div className="text-center mb-7">
+          <button
+            type="button"
+            onClick={(e) => {
+              setText(e.target.textContent);
+              setActiveBtn('created');
+            }}
+            className={`${activeBtn === 'created' ? activeBtnStyles : notActiveBtnStyles}`}
+          >
+            Created
+          </button>
+          <button
+            type="button"
+            onClick={(e) => {
+              setText(e.target.textContent);
+              setActiveBtn('saved');
+            }}
+            className={`${activeBtn === 'saved' ? activeBtnStyles : notActiveBtnStyles}`}
+          >
+            Saved
+          </button>
+        </div>
+
+        <div className="px-2">
+          <MasonaryLayout pins={pins} />
+        </div>
+
+        {pins?.length === 0 && (
+        <div className="flex justify-center font-bold items-center w-full text-1xl mt-2">
+          No Pins Found!
+        </div>
+        )}
         </div>
     </div>
   )
